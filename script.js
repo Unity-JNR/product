85d4b2a411f387555b529ab2435a398b92726207
Unity-JNR <unitydemas12@gmail.com>
2023-11-14 12:55:27 +0200

    added java script

---
+++ script.js
@@ -0,0 +1,2 @@
+
+    window.alert("I will be conducting sales on campus. If you prefer to make an electronic funds transfer for your purchase, please feel free to contact me via Slack for my banking details")
