b59540e90c37ea5f192afac60e1d1d9294b15c01
UnityDemas <98840485+UnityDemas@users.noreply.github.com>
2023-11-18 15:53:58 +0200

    added form order

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 
+
+    window.alert('please read the alerts')
     window.alert("I will be conducting sales on campus. If you prefer to make an electronic funds transfer for your purchase, please feel free to contact me via Slack for my banking details")
+    window.alert('scroll to the bottom to place a order')
+  
